8e9cbea0bf5118b663a40a915ae4b015b1b32293
HariharanT99 <hariharant99@gmail.com>
2021-07-22 22:56:45 +0530

    22-July-2021 Assignments

---
+++ 22-July-2021/Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManipulation assignObj = new StringManipulation();
+
+            assignObj.MyString = "This is my demo string";
+            assignObj.AddString = "My second demo";
+            assignObj.CharOccurence = 's';
+
+
+            // Adding methods to delegate
+            Console.WriteLine("\nReplace Space by underscore\n");
+            StringManipulation.StringDelegate stringDelegate = assignObj.ReplaceSpace;
+
+            Console.WriteLine("\n\nReverse the string\n");
+            stringDelegate += assignObj.Reverse;
+
+            Console.WriteLine("\n\nJoin String\n");
+            stringDelegate += assignObj.StringJoin;
+
+            Console.WriteLine("\n\nFind the Occurance\n");
+            stringDelegate += assignObj.Occurrence;
+
+
+            assignObj.StringProcess(stringDelegate);
+        }
+    }
+}
---
+++ 22-July-2021/Delegates/StringManipulation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class StringManipulation
+    {
+        public delegate void StringDelegate();          //Deligate
+
+        private string _myString;
+        private string _addString;
+        private char _charOccurence;
+        public string MyString 
+        { 
+            set { this._myString = value; } 
+        }
+
+        public string AddString 
+        { 
+            set { this._addString = value; }
+        }
+
+        public char CharOccurence 
+        {  
+            set { this._charOccurence = value; }
+        }
+
+        public void ReplaceSpace()
+        {
+            var str = _myString.Replace(' ', '_');
+            Console.WriteLine(str);
+        }
+
+        public void Reverse()
+        {
+            char[] chars = new char[_myString.Length];
+            for (int i = 0, j = _myString.Length - 1; i <= j; i++, j-- )
+            {
+                chars[i] = _myString[j];
+                chars[j] = _myString[i];
+            }
+
+            foreach (var i in chars)
+            {
+                Console.Write(i);
+            }
+        }
+
+        public void StringJoin()
+        {
+            var strJoin = _myString + _addString;
+            Console.WriteLine(strJoin);
+        }
+
+        public void Occurrence()
+        {
+            int count = 0;
+            int index;
+            var myStringList = _myString.ToCharArray();
+
+            if (_myString.Contains(_charOccurence))
+            {
+                index = _myString.IndexOf(_charOccurence);
+                Console.WriteLine($"Char: {_charOccurence},Position: {index}");
+            }
+
+            foreach (var c in myStringList)
+            {
+                if (c == _charOccurence)
+                    count += 1;
+                else
+                    continue;
+            }
+            Console.WriteLine($"Char: {_charOccurence}, Occurences: {count}");
+        }
+        public void StringProcess(StringDelegate stringDelegate)                    //Method for invoke delegate
+        {
+            stringDelegate();
+        }
+    }
+}
