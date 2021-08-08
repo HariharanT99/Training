4d7482576bafcc7106532ef40843c47c3c11c135
HariharanT99 <hariharant99@gmail.com>
2021-08-08 21:22:56 +0530

    Assignments

---
+++ 06-Aug-2021/TodoApp/src/app/todo/todo.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  taskList: string[] = [];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onLoadDisplay(){
+    this.taskList = JSON.parse(sessionStorage.getItem('taskList') || '{}');
+    console.log(2)
+    console.log(this.taskList)
+  }
+
+  onAdd(addTask: any){ 
+    this.taskList.push(addTask.value)
+    addTask.value = null
+    sessionStorage.setItem('taskList',JSON.stringify(this.taskList))
+  }
+
+  onDone(index: number){
+    this.taskList.forEach(task => {
+      if (this.taskList[index] == task)
+      {
+        this.taskList[index]= `✔ ${task}`
+      }
+    });
+  }
+
+  onRemove(index: number){
+    this.taskList.splice(index, 1)
+    sessionStorage.setItem('taskList',JSON.stringify(this.taskList))
+  }
+
+}
