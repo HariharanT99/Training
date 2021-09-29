f75491209e9c5749731f4ea9284a45a6ffab0dc4
HariharanT99 <hariharant99@gmail.com>
2021-09-29 21:34:07 +0530

    Register

---
+++ External/MVC/29-Sep-2021/DataAccess/Access/AccountDAL.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BussinessObject.Model;
+
+namespace DataAccess.Access
+{
+    public class AccountDAL
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AccountDAL(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this._userManager = userManager;
+        }
+
+        public async Task<IdentityResult> CreateUser(Register model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            return result;
+        }
+
+    }
+}
---
+++ External/MVC/29-Sep-2021/EmployeeManagement/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BussinessObject.Model;
+using DataAccess.Access;
+using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AccountDAL _accountDAL;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(AccountDAL accountDAL, SignInManager<IdentityUser> signInManager)
+        {
+            this._accountDAL = accountDAL;
+            this._signInManager = signInManager;
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _accountDAL.CreateUser(model);
+
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(String.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+    }
+}
